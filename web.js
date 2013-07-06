8517fdd74f2200e10a130660f03711ae9ff1564f
Kurt <kespindler@gmail.com>
2013-07-06 15:39:37 +0200

    buffer read from file works

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require('express'),
+    fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.get('/', function(request, res) {
+  var fileBuffer = fs.readFileSync('index.html');
+  res.send(fileBuffer.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
